e942d47c05b5a4b3be6cbb04f85ecb5265d420da
sagnikghosh1999 <saggywebd@gmail.com>
2024-05-12 23:00:03 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,9 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
   theme: {
     extend: {
       backgroundImage: {
